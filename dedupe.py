68e49486919c596ec1fd72614cf1733cc3bfa0a3
Matt Iggo <matt@iggo.co.uk>
2019-07-11 21:42:15 +0100

    Update dedupe.py
    Update dedupe.py to use the new IA CDX API endpoint

--- dedupe.py
+++ dedupe.py
@@ -1,46 +1,68 @@
 # encoding=utf8
 import sys
-import os
-import hashlib
 import re
 import time
 import requests
-
-
+import random
 import warcio
 from warcio.archiveiterator import ArchiveIterator
 from warcio.warcwriter import WARCWriter
+from datetime import datetime
 
 assert hasattr(warcio, 'ATWARCIO'), 'warcio was not imported correctly. Location: ' + warcio.__file__
 
 def ia_available(url, digest):
-    tries = 0
     print('Deduplicating digest ' + digest + ', url ' + url)
     assert digest.startswith('sha1:')
     digest = digest.split(':', 1)[1]
-    encoded = digest + ';' + re.sub('^https?://', '', url)
-    encoded = encoded.encode('utf-8')
-    hashed = hashlib.sha256(encoded).hexdigest()
-    while tries < 10:
+    verification_re = re.compile('^\d{14}\ http?s://')
+    tries = 0
+    delay = 1
+    while True:
         try:
+            if tries <= 9:
+                pass
+            else:
+                raise Exception('Internet Archive CDX API Offline - Aborting')
             tries += 1
-            ia_data = requests.get('http://NewsGrabberDedupe.b-cdn.net/?key={hashed}' \
-                                   .format(hashed=hashed), timeout=60)
-            if not ';' in ia_data.text:
-                return False
-            return ia_data.text.split(';', 1)
-        except:
-            pass
-            time.sleep(1)
-
+            ia_data = requests.get(
+                'http://wwwb-dedup.us.archive.org/cdx/search/cdx?url={url}&gzip=false&limit=1&filter=digest:{digest}&fl=timestamp,original&to=201905310000' \
+                .format(url=url, digest=digest), timeout=3)
+        except requests.ConnectionError as error:
+                print("Error connecting to web.archive.org/cdx/search - Sleeping for " + str(round(delay, 2)) + " seconds.")
+                time.sleep(delay)
+                delay += delay + (random.randint(0,1000)/1000)
+                continue
+        break
+    if re.search(verification_re, str(ia_data.text)):
+        return ia_data.text.split(' ', 1)
+    tries = 0
+    delay = 1
+    while True:
+        try:
+            if tries <= 9:
+                pass
+            else:
+                raise ValueError('Internet Archive CDX API Offline - Aborting')
+                exit(1)
+            ia_data = requests.get(
+                'http://wwwb-dedup.us.archive.org/cdx/search/cdx?url={url}&gzip=false&limit=1&filter=digest:{digest}&fl=timestamp,original&from=20190703000' \
+                .format(url=url, digest=digest), timeout=3)
+        except requests.ConnectionError as error:
+            print("Error connecting to web.archive.org/cdx/search - Sleeping for " + str(round(delay, 2)) + " seconds.")
+            time.sleep(delay)
+            delay += delay + (random.randint(0, 1000) / 1000)
+            continue
+        break
+    if re.search(verification_re, str(ia_data.text)):
+        return ia_data.text.split(' ', 1)
     return False
 
 def revisit_record(writer, record, ia_record):
     warc_headers = record.rec_headers
-    #warc_headers.add_header('WARC-Refers-To'
     warc_headers.replace_header('WARC-Refers-To-Date',
-        '-'.join([ia_record[0][:4], ia_record[0][4:6], ia_record[0][6:8]]) + 'T' + 
-        ':'.join([ia_record[0][8:10], ia_record[0][10:12], ia_record[0][12:14]]) + 'Z')
+                                '-'.join([ia_record[0][:4], ia_record[0][4:6], ia_record[0][6:8]]) + 'T' +
+                                ':'.join([ia_record[0][8:10], ia_record[0][10:12], ia_record[0][12:14]]) + 'Z')
     warc_headers.replace_header('WARC-Refers-To-Target-URI', ia_record[1])
     warc_headers.replace_header('WARC-Type', 'revisit')
     warc_headers.replace_header('WARC-Truncated', 'length')
@@ -56,6 +78,9 @@ def revisit_record(writer, record, ia_record):
     )
 
 def process(filename_in, filename_out):
+    starttime = datetime.now()
+    dedupemiss = 0
+    dedupehit = 0
     with open(filename_in, 'rb') as file_in:
         with open(filename_out, 'wb') as file_out:
             writer = WARCWriter(filebuf=file_out, gzip=True)
@@ -64,16 +89,20 @@ def process(filename_in, filename_out):
                     record_url = record.rec_headers.get_header('WARC-Target-URI')
                     record_digest = record.rec_headers.get_header('WARC-Payload-Digest')
                     ia_record = ia_available(record_url, record_digest)
-                    #print(ia_record)
                     if not ia_record:
                         writer.write_record(record)
                     else:
                         print('Found duplicate, writing revisit record.')
                         writer.write_record(revisit_record(writer, record, ia_record))
+                        dedupehit = dedupehit + 1
                 else:
                     writer.write_record(record)
+                    dedupemiss = dedupemiss + 1
+    print(str(dedupehit) + " Hits")
+    print(str(dedupemiss) + " Misses")
+    print("took " + str(datetime.now() - starttime) + " to execute")
 
 if __name__ == '__main__':
     filename_in = sys.argv[1]
     filename_out = sys.argv[2]
-process(filename_in, filename_out)
+    process(filename_in, filename_out)
