339df31e41d299e855172d15b5a9314d56a1f981
Matt Iggo <matt@iggo.co.uk>
2019-07-12 08:50:24 +0100

    Update pipeline.py
    Bump pipeline version after deduplication changes.

--- pipeline.py
+++ pipeline.py
@@ -95,7 +95,7 @@ if not YOUTUBE_DL_EXE:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = "20190711.01"
+VERSION = "20190712.01"
 TRACKER_ID = 'newsgrabber'
 TRACKER_HOST = 'tracker.archiveteam.org'
 
